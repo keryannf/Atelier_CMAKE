18ffdbbdcc969bc14c9d95cfb5b9f7ca5ee1fcb8
keryannf <104188424+keryannf@users.noreply.github.com>
2025-12-12 15:01:24 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,40 +3,34 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]) {
-
-    if (argc < 3 || argc > 4) {
-        printf("Erreur de parametres");
-        return 0;
-    }
-
-    char* op = argv[1];
+int main(int argc, char argv[]){
+    if (argc == 4)
+    {
+    char op = argv[1];
+    char* a = argv[2];
+    char* b = argv[3];
     double r = 0;
-
-    if (strcmp(op, "car") == 0) {
-        // fonction à UN seul argument
-        r = _car(atof(argv[2]));
-        printf("%lf", r);
+    if (strcmp(op, "add") == 0) {
+        r = _add(atof(a), atof(b));
+        printf("%lf",r);
     }
-    else if (argc == 4) {
-        // opérations à DEUX arguments
-        double a = atof(argv[2]);
-        double b = atof(argv[3]);
-
-        if (strcmp(op, "add") == 0) r = _add(a, b);
-        else if (strcmp(op, "sub") == 0) r = _sub(a, b);
-        else if (strcmp(op, "mul") == 0) r = _mul(a, b);
-        else if (strcmp(op, "div") == 0) r = _div(a, b);
-        else {
-            printf("Erreur de parametres");
-            return 0;
-        }
-
-        printf("%lf", r);
+    else if (strcmp(op, "sub") == 0) {
+        r = _sub(atof(a), atof(b));
+        printf("%lf",r);
     }
-    else {
-        printf("Erreur de parametres");
+    else if (strcmp(op, "mul") == 0) {
+        r = _mul(atof(a), atof(b));
+        printf("%lf",r);
     }
-
+    else if (strcmp(op, "div") == 0) {
+        r = _div(atof(a), atof(b));
+        printf("%lf",r);
+    }
+    else if (strcmp(argv[1], "car") == 0) {
+    printf("%d\n", car(atoi(argv[2])));
+    }
+    else {printf("Erreur de parametres");}
+    }
+    else {printf("Erreur de parametres");}
     return 0;
 }
